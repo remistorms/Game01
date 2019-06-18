91fbd36e58b2c5cee3d8ff06f055feaeb492e407
Remi Storms <ultimate.spidey.18@gmail.com>
2019-06-18 00:41:45 -0500

    MapManager
    Created Map Manager
    
    Available Methods:
    ClearMap();
    GenerateMap();
    GetNeighbours();

---
+++ G01_Unity_2018_4_1_f_1/Assets/Map_Manager.cs
@@ -0,0 +1,123 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Map_Manager : MonoBehaviour
+{
+
+    [SerializeField] private int mapSize = 4;
+    [SerializeField] private float tileSize = 10;
+    [SerializeField] private GameObject HomeTile;
+    [SerializeField] private GameObject[] RandomTiles;
+
+    public List<GameObject> allMapTiles;
+    public Dictionary<Vector3, GameObject> TileCoordinatesDictionary;
+
+    public void ClearMap()
+    {
+        //Destroys previous map
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            Destroy(transform.GetChild(i).gameObject);
+        }
+        TileCoordinatesDictionary = new Dictionary<Vector3, GameObject>();
+        allMapTiles.Clear();
+    }
+
+    public void GenerateMap()
+    {
+
+
+        ClearMap();
+
+        GameObject selectedTile;
+        //Create the map tiles from -mapSize, to mapSize
+        for (int x = -mapSize; x < mapSize +1; x++)
+        {
+            for (int y = -mapSize; y < mapSize +1; y++)
+            {
+                Vector3 position = new Vector3( x * tileSize, 0, y * tileSize );
+                Vector3 mapCoordinate = new Vector3(x, 0, y);
+
+                if (x == 0 && y == 0)
+                {
+                    // use home tile
+                    selectedTile = HomeTile;
+                }
+                else
+                {
+                    //use random tile
+                    int random = Random.Range(0, RandomTiles.Length);
+                    selectedTile = RandomTiles[random];
+                }
+
+                GameObject instantiatedTile = Instantiate(selectedTile, this.transform) as GameObject;
+
+                allMapTiles.Add(instantiatedTile);
+                TileCoordinatesDictionary.Add(mapCoordinate, instantiatedTile);
+
+                instantiatedTile.transform.localPosition = position;
+            }
+        }
+    }
+
+    public List<GameObject> GetNeigbouringTiles (Vector3 coordinate)
+    {
+        List<GameObject> neighbourgs = new List<GameObject>();
+        //check if coordinate exists inside the dictionary
+        if (TileCoordinatesDictionary.ContainsKey(coordinate))
+        {
+            //Get Up Neighbour
+            Vector3 northCoordinate = new Vector3(coordinate.x, 0, coordinate.z + 1);
+            GameObject northTile; 
+            TileCoordinatesDictionary.TryGetValue( northCoordinate, out northTile );
+
+            //Get South Neighbour
+            Vector3 southCoordinate = new Vector3(coordinate.x, 0, coordinate.z - 1);
+            GameObject southTile;
+            TileCoordinatesDictionary.TryGetValue(southCoordinate, out southTile);
+
+            //Get West Neighbour
+            Vector3 westCoordinate = new Vector3(coordinate.x - 1, 0, coordinate.z);
+            GameObject westTile;
+            TileCoordinatesDictionary.TryGetValue(westCoordinate, out westTile);
+
+            //Get East Neighbour
+            Vector3 eastCoordinate = new Vector3(coordinate.x + 1, 0, coordinate.z);
+            GameObject eastTile;
+            TileCoordinatesDictionary.TryGetValue(eastCoordinate, out eastTile);
+
+            //Get SouthEast Neighbour
+            Vector3 seCoordinate = new Vector3(coordinate.x + 1, 0, coordinate.z - 1);
+            GameObject seTile;
+            TileCoordinatesDictionary.TryGetValue(seCoordinate, out seTile);
+
+            //Get SouthWest Neighbour
+            Vector3 swCoordinate = new Vector3(coordinate.x - 1, 0, coordinate.z - 1);
+            GameObject swTile;
+            TileCoordinatesDictionary.TryGetValue(swCoordinate, out swTile);
+
+            //Get NorthEast Neighbour
+            Vector3 neCoordinate = new Vector3(coordinate.x + 1, 0, coordinate.z + 1);
+            GameObject neTile;
+            TileCoordinatesDictionary.TryGetValue(neCoordinate, out neTile);
+
+            //Get NorthWest Neighbour
+            Vector3 nwCoordinate = new Vector3(coordinate.x - 1, 0, coordinate.z + 1);
+            GameObject nwTile;
+            TileCoordinatesDictionary.TryGetValue(nwCoordinate, out nwTile);
+
+            neighbourgs.Add(northTile);
+            neighbourgs.Add(neTile);
+            neighbourgs.Add(eastTile);
+            neighbourgs.Add(seTile);
+            neighbourgs.Add(southTile);
+            neighbourgs.Add(swTile);
+            neighbourgs.Add(westTile);
+            neighbourgs.Add(nwTile);
+        }
+
+        return neighbourgs;
+    }
+
+}
